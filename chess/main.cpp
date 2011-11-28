0513d963badd35a1be247b69d168024b4e98d70f
Lennon Chimbumu <lennonc@stanford.edu>
2011-11-27 17:30:31 -0800

    Initial Commit

---
+++ chess/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by Lennon Chimbumu on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
